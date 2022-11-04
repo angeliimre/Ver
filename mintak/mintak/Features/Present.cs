c9e1266fad7e49ecab2919882db1483d17c0ca48
Angeli Imre <angeliimre577@gmail.com>
2022-11-04 20:28:33 +0100

    finish

--- mintak/mintak/Features/Present.cs
+++ mintak/mintak/Features/Present.cs
@@ -1,19 +1,22 @@
-﻿using System;
+﻿using mintak.Abstractions;
+using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using mintak.Abstractions;
 
 namespace mintak.Features
 {
-    internal class Car : Toy
+    public class Present : Toy
     {
+        public Present(Color ribbon,Color box)
+        {
+
+        }
         protected override void DrawImage(Graphics g)
         {
-            Image imageFile = Image.FromFile("Images/car.png");
-            g.DrawImage(imageFile, new Rectangle(0, 0, Width, Height));
+            throw new NotImplementedException();
         }
     }
 }
--- mintak/mintak/Form1.cs
+++ mintak/mintak/Form1.cs
@@ -14,13 +14,14 @@ namespace mintak
 {
     public partial class Form1 : Form
     {
+        private Toy _nextToy;
         private List<Toy> _toys = new List<Toy>();
         private IToyFactory _factory;
 
         public IToyFactory Factory
         {
             get { return _factory; }
-            set { _factory = value; }
+            set { _factory = value; DisplayNext(); }
         }
 
         public Form1()
@@ -54,6 +55,45 @@ namespace mintak
                 _toys.Remove(elso);
                 mainPanel.Controls.Remove(elso);
             }
+        }
+
+        private void mainPanel_Paint(object sender, PaintEventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Factory = new CarFactory();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Factory = new BallFactory
+            {
+                BallColor = button3.BackColor
+            };
+        }
+
+        private void DisplayNext()
+        {
+            if (_nextToy != null)
+                Controls.Remove(_nextToy);
+            _nextToy = Factory.CreateNew();
+            _nextToy.Top = label1.Top + label1.Height + 20;
+            _nextToy.Left = label1.Left;
+            Controls.Add(_nextToy);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            var button = (Button)sender;
+            var colorPicker = new ColorDialog();
+
+            colorPicker.Color = button.BackColor;
+            if (colorPicker.ShowDialog() != DialogResult.OK)
+                return;
+            button.BackColor = colorPicker.Color;
         }
     }
 }
